7bb0b622f3b5437e7def7e33ff9739b69b057330
Andrew Jones <andrewjones86@googlemail.com>
2012-05-12 10:52:17 +0100

    gits being stupid

--- BuildContrib/lib/Foswiki/Contrib/BuildContrib/Targets/pod.pm
+++ BuildContrib/lib/Foswiki/Contrib/BuildContrib/Targets/pod.pm
@@ -15,24 +15,41 @@
 #
 package Foswiki::Contrib::Build;
 
-=begin TML
+# Build POD documentation. This target defines =%$POD%= - it
+# does not generate any output. The target will be invoked
+# automatically if =%$POD%= is used in a .txt file. POD documentation
+# is intended for use by developers only.
 
----++++ target_pod
+# POD text in =.pm= files should use TML syntax or HTML. Packages should be
+# introduced with a level 1 header, ---+, and each method in the package by
+# a level 2 header, ---++. Make sure you document any global variables used
+# by the module.
 
-Print POD documentation. This target does not modify any files, it simply
-prints the (TML format) POD.
-
-POD text in =.pm= files should use TML syntax or HTML. Packages should be
-introduced with a level 1 header, ---+, and each method in the package by
-a level 2 header, ---++. Make sure you document any global variables used
-by the module.
-
-=cut
-
-sub target_pod {
+sub target_POD {
     my $this = shift;
-    $this->target_POD();
-    print $this->{POD} . "\n";
+    $this->{POD} = '';
+    local $/ = "\n";
+    foreach my $file ( @{ $this->{files} } ) {
+        my $pmfile = $file->{name};
+        if ( $pmfile =~ /\.p[ml]$/o ) {
+            next if $pmfile =~ /^$this->{project}_installer(\.pl)?$/;
+            $pmfile = $this->{basedir} . '/' . $pmfile;
+            open( PMFILE, '<', $pmfile ) || die $!;
+            my $inPod = 0;
+            while ( my $line = <PMFILE> ) {
+                if ( $line =~ /^=(begin|pod)/ ) {
+                    $inPod = 1;
+                }
+                elsif ( $line =~ /^=cut/ ) {
+                    $inPod = 0;
+                }
+                elsif ($inPod) {
+                    $this->{POD} .= $line;
+                }
+            }
+            close(PMFILE);
+        }
+    }
 }
 
 1;
